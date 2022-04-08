a9b0b52484d2c9593e44475ef7087430ff6c29cb
Ivan Pizarroso <ivnp31@gmail.com>
2022-04-08 14:06:52 +0200

    image random add

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@ let credit = 10;
 
 const images = ["1.png", "2.png", "3.png", "4.png", "5.png"];
 
+let image1 = document.createElement("img");
+image1.setAttribute("src", "images/inicio.png");
+
+let image2 = document.createElement("img");
+image2.setAttribute("src", "images/inicio.png");
+
+let image3 = document.createElement("img");
+image3.setAttribute("src", "images/inicio.png");
+
 const figure1 = document.querySelector(".figure1");
+figure1.appendChild(image1);
 const figure2 = document.querySelector(".figure2");
+figure2.appendChild(image2);
 const figure3 = document.querySelector(".figure3");
+figure3.appendChild(image3);
 
 const buttonBottomLeft = document.querySelector(".bottom1");
 const buttonBottomCenter = document.querySelector(".bottom2");
@@ -35,29 +47,43 @@ const credits = (credit) => {
   }
 };
 
+const imageRandom = (image) => {
+  let numberRandom = Math.floor(Math.random() * 5);
+  let imageRandom = images[numberRandom];
+
+  image.setAttribute("src", `images/${imageRandom}`);
+};
+
 // Events
 buttonStart.addEventListener("click", () => {
   credit -= 1;
   coin.textContent = `Credits: ${credit} $`;
   credits(credit);
+
+  imageRandom(image1);
+  imageRandom(image2);
+  imageRandom(image3);
 });
 
 buttonBottomLeft.addEventListener("click", () => {
   credit -= 1;
   coin.textContent = `Credits: ${credit} $`;
   credits(credit);
+  imageRandom(image1);
 });
 
 buttonBottomCenter.addEventListener("click", () => {
   credit -= 1;
   coin.textContent = `Credits: ${credit} $`;
   credits(credit);
+  imageRandom(image2);
 });
 
 buttonBottomRight.addEventListener("click", () => {
   credit -= 1;
   coin.textContent = `Credits: ${credit} $`;
   credits(credit);
+  imageRandom(image3);
 });
 
 console.log();
