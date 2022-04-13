dccd666992f0ecf4b1374101483125780f906ff4
Ivan Pizarroso <ivnp31@gmail.com>
2022-04-13 13:40:28 +0200

    error

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { awards } from "./awards.js";
+
 const buttonStart = document.querySelector(".button");
 
 const showModal = (cred) => {
@@ -99,74 +101,76 @@ const imageRandom = (image) => {
   image.setAttribute("src", `images/${imageRandom}`);
 };
 
-const awards = (imageLeft, imageCenter, imageRight) => {
-  const imageSrcLeft = imageLeft.attributes.src.textContent;
-  const imageSrcCenter = imageCenter.attributes.src.textContent;
-  const imageSrcRight = imageRight.attributes.src.textContent;
-  const sun = "images/1.png";
-  const women = "images/2.png";
-  const diamond = "images/3.png";
-  const toad = "images/4.png";
-  const heart = "images/5.png";
-  const moon = "images/6.png";
-  const star = "images/7.png";
+awards(imageLeft, imageCenter, imageRight);
 
-  if (imageSrcLeft === sun) {
-    if (imageSrcCenter === sun) {
-      if (imageSrcRight === sun) {
-        showModal(5);
-      }
-    }
-  }
+// const awards = (imageLeft, imageCenter, imageRight) => {
+//   const imageSrcLeft = imageLeft.attributes.src.textContent;
+//   const imageSrcCenter = imageCenter.attributes.src.textContent;
+//   const imageSrcRight = imageRight.attributes.src.textContent;
+//   const sun = "images/1.png";
+//   const women = "images/2.png";
+//   const diamond = "images/3.png";
+//   const toad = "images/4.png";
+//   const heart = "images/5.png";
+//   const moon = "images/6.png";
+//   const star = "images/7.png";
 
-  if (imageSrcLeft === women) {
-    if (imageSrcCenter === women) {
-      if (imageSrcRight === women) {
-        showModal(10);
-      }
-    }
-  }
+//   if (imageSrcLeft === sun) {
+//     if (imageSrcCenter === sun) {
+//       if (imageSrcRight === sun) {
+//         showModal(5);
+//       }
+//     }
+//   }
 
-  if (imageSrcLeft === diamond) {
-    if (imageSrcCenter === diamond) {
-      if (imageSrcRight === diamond) {
-        showModal(15);
-      }
-    }
-  }
+//   if (imageSrcLeft === women) {
+//     if (imageSrcCenter === women) {
+//       if (imageSrcRight === women) {
+//         showModal(10);
+//       }
+//     }
+//   }
 
-  if (imageSrcLeft === toad) {
-    if (imageSrcCenter === toad) {
-      if (imageSrcRight === toad) {
-        showModal(20);
-      }
-    }
-  }
+//   if (imageSrcLeft === diamond) {
+//     if (imageSrcCenter === diamond) {
+//       if (imageSrcRight === diamond) {
+//         showModal(15);
+//       }
+//     }
+//   }
 
-  if (imageSrcLeft === heart) {
-    if (imageSrcCenter === heart) {
-      if (imageSrcRight === heart) {
-        showModal(25);
-      }
-    }
-  }
+//   if (imageSrcLeft === toad) {
+//     if (imageSrcCenter === toad) {
+//       if (imageSrcRight === toad) {
+//         showModal(20);
+//       }
+//     }
+//   }
 
-  if (imageSrcLeft === moon) {
-    if (imageSrcCenter === moon) {
-      if (imageSrcRight === moon) {
-        showModal(30);
-      }
-    }
-  }
+//   if (imageSrcLeft === heart) {
+//     if (imageSrcCenter === heart) {
+//       if (imageSrcRight === heart) {
+//         showModal(25);
+//       }
+//     }
+//   }
 
-  if (imageSrcLeft === star) {
-    if (imageSrcCenter === star) {
-      if (imageSrcRight === star) {
-        showModal(35);
-      }
-    }
-  }
-};
+//   if (imageSrcLeft === moon) {
+//     if (imageSrcCenter === moon) {
+//       if (imageSrcRight === moon) {
+//         showModal(30);
+//       }
+//     }
+//   }
+
+//   if (imageSrcLeft === star) {
+//     if (imageSrcCenter === star) {
+//       if (imageSrcRight === star) {
+//         showModal(35);
+//       }
+//     }
+//   }
+// };
 
 buttonReload.addEventListener("click", () => window.location.reload());
 
